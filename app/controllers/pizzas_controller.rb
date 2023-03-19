d8755f0bca23f7c108311304674eb74f5936f74e
abdishukri <abdishukrimohamed105@gmail.com>
2023-03-20 01:17:10 +0300

    set up controller methods

--- app/controllers/pizzas_controller.rb
+++ app/controllers/pizzas_controller.rb
@@ -1,4 +1,8 @@
 class PizzasController < ApplicationController
 
     
+    def index
+        pizzas = Pizza.all
+        render json: pizzas.to_json(except: [:created_at, :updated_at])
+    end
 end
--- app/controllers/restaurant_pizzas_controller.rb
+++ app/controllers/restaurant_pizzas_controller.rb
@@ -1,4 +1,25 @@
 class RestaurantPizzasController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
-    
+    def index
+        restaurantspizza = RestaurantPizza.all
+        render json: restaurantspizza
+    end
+
+    def create
+        restaurant_pizza =  RestaurantPizza.create!(restaurantpizza_params)
+        render json: restaurant_pizza.pizza.to_json(except: [:created_at, :updated_at]), status: :created
+    end
+
+private
+
+  def restaurantpizza_params
+    params.permit(:price, :pizza_id, :restaurant_id)
+  end
+
+  def render_unprocessable_entity_response(invalid)
+    render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+  end
+
 end
